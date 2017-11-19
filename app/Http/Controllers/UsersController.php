351ae05f29d3ff0156fd5d418f5fd9728485bee5
karin1217 <1946294910@qq.com>
2017-11-19 11:58:06 +0000

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -15,11 +16,12 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
-        return view('users.edit');
+        return view('users.edit', compact('user'));
     }
 
-    public function update(User $user)
+    public function update(UserRequest $request, User $user)
     {
-
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success','个人资料更新成功！');
     }
 }
---
+++ database/migrations/2017_11_19_002408_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //要添加的字段信息
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //删除字段
+            $table->dropColumn('avatar');
+            $table->dropColumn('introduction');
+        });
+    }
+}
