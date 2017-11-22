dfeb1309e43a049ca486c2abd4f366fa762304ad
karin1217 <1946294910@qq.com>
2017-11-22 06:51:09 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,10 +14,10 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
-	    //使用 with 进行预加载，解决 N + 1 问题
-		$topics = Topic::with('user','category')->paginate(30);
+
+		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
