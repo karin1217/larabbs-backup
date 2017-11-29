99afdff6edff32e6d122c3b593f6e8f7d615da5c
karin1217 <1946294910@qq.com>
2017-11-29 18:26:19 +0000

    Git config core.autocrlf false

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,7 @@ class User extends Authenticatable
         $this->attributes['password'] = $value;
     }
 
+    // 定义 Eloquent 修改器
     public function setAvatarAttribute($path)
     {
         // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
